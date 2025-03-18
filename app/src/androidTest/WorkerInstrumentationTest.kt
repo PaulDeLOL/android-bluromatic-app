4f30a2ed87275dfa675b80f10f4bdbb70f869170
PaulDeLOL <pjg22b@fsu.edu>
2025-03-18 18:08:15 -0500

    Commit to upload finished project to GitHub

---
+++ app/src/androidTest/WorkerInstrumentationTest.kt
@@ -0,0 +1,34 @@
+class WorkerInstrumentationTest {
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun cleanupWorker_doWork_resultSuccess() {
+        val worker = TestListenableWorkerBuilder<CleanupWorker>(context).build()
+        runBlocking {
+            val result = worker.doWork()
+            assertTrue(result is ListenableWorker.Result.Success)
+        }
+    }
+
+    @Test
+    fun saveImageToFileWorker_doWork_resultSuccessReturnsUrl() {
+        val worker = TestListenableWorkerBuilder<SaveImageToFileWorker>(context)
+            .setInputData(workDataOf(mockUriInput))
+            .build()
+        runBlocking {
+            val result = worker.doWork()
+            val resultUri = result.outputData.getString(KEY_IMAGE_URI)
+            assertTrue(result is ListenableWorker.Result.Success)
+            assertTrue(result.outputData.keyValueMap.containsKey(KEY_IMAGE_URI))
+            assertTrue(
+                resultUri?.startsWith("content://media/external/images/media/")
+                    ?: false
+            )
+        }
+    }
+}
